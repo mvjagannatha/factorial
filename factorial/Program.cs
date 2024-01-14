4ff9c08eef0c85717f747e0196fef26ab64226c2
Admin <Admin@DESKTOP-6PQE64G>
2024-01-14 11:42:57 +0530

    Add project files.

---
+++ factorial/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace factorialapp
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1;
+            for(i=1;i<=5;i++)
+            {
+                fact=fact*i;
+            }
+            Console.WriteLine(fact);
+        }
+        
+    }
+}
